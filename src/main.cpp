4c12888ac484b0ae4e7fbfc1ea9d805abd995a35
root <root@DESKTOP-5G1FMJC.>
2025-03-13 23:07:32 +0000

    Bug | Output created at root

--- src/main.cpp
+++ src/main.cpp
@@ -8,7 +8,7 @@ int main(int argc, char* argv[]) {
     if (argc > 1) {
         // Batch mode: Process input file and create output file
         std::string inputFile = argv[1];
-        std::string outputFile = (argc > 2) ? argv[2] : "output.txt"; // Use given output file or default
+        std::string outputFile = (argc > 2) ? argv[2] : "../output.txt"; // Use given output file or default
 
         std::cout << "Running in batch mode with input file: " << inputFile << std::endl;
         std::cout << "Output will be written to: " << outputFile << std::endl;
