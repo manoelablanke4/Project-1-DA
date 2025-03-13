e073db216b402d05841527b2573138d6c5c733f0
root <root@DESKTOP-5G1FMJC.>
2025-03-13 15:11:31 +0000

    Bug correction

--- Functions/restrictedRoutePlanning.cpp
+++ Functions/restrictedRoutePlanning.cpp
@@ -26,11 +26,13 @@ void excludeNodesOrSegments(int origin, int destination,
         int toID   = seg.second;
 
         Vertex<Location>* fromV = nullptr;
+        // check if fromID is known
         if (idmap.find(fromID) != idmap.end()) {
             fromV = idmap[fromID];
         }
-        if (!fromV) continue;
+        if (!fromV) continue; // if we didn't find a vertex or it's null, skip
 
+        // Mark the edge (fromID -> toID) as ignored
         for (auto e : fromV->getAdj()) {
             if (e->getDest()->getInfo().id == toID) {
                 e->setIgnored(true);
@@ -38,33 +40,65 @@ void excludeNodesOrSegments(int origin, int destination,
         }
     }
 
-    double time = 0.0;
-    dijkstra(&cityGraph, origin, localIgnoreVertex);
+    // No include input
+    if (include == -1) {
+        double time = 0.0;
+        dijkstra(&cityGraph, origin, localIgnoreVertex);
 
-    // We'll declare bestPath
-    std::vector<int> bestPath;
+        std::vector<int> bestPath = getBestPath(&cityGraph, origin, destination, time);
+        if (bestPath.empty()) {
+            std::cout << "No Path Found" << std::endl;
+            return;
+        }
 
-    // If include != -1 (no input), let's get bestPath from 'origin' to 'include'
-    // otherwise from 'origin' to 'destination'
-    if (include != -1) {
-        bestPath = getBestPath(&cityGraph, origin, include, time);
-    } else {
-        bestPath = getBestPath(&cityGraph, origin, destination, time);
-    }
-
-    if (bestPath.empty()) {
-        std::cout << "No Path Found" << std::endl;
+        std::cout << "Best Path: ";
+        for (size_t i = 0; i < bestPath.size(); i++) {
+            std::cout << bestPath[i];
+            if (i < bestPath.size() - 1) {
+                std::cout << "->";
+            }
+        }
+        std::cout << "\nTime: " << time << std::endl;
         return;
     }
 
-    std::cout << "Best Path: ";
-    for (size_t i = 0; i < bestPath.size(); i++) {
-        std::cout << bestPath[i];
-        if (i < bestPath.size() - 1) {
+    // ------------------------------------------------------------------
+    // If include != -1, we want: origin -> include -> destination
+    // ------------------------------------------------------------------
+
+    // 1) origin -> include
+    double firstTime = 0.0;
+    dijkstra(&cityGraph, origin, localIgnoreVertex);
+    std::vector<int> firstPath = getBestPath(&cityGraph, origin, include, firstTime);
+
+    if (firstPath.empty()) {
+        std::cout << "No Path Found (origin -> include)" << std::endl;
+        return;
+    }
+
+
+    // 3) include -> destination
+    double secondTime = 0.0;
+    dijkstra(&cityGraph, include, localIgnoreVertex);
+    std::vector<int> secondPath = getBestPath(&cityGraph, include, destination, secondTime);
+
+    if (secondPath.empty()) {
+        std::cout << "No Path Found (include -> destination)" << std::endl;
+        return;
+    }
+
+    secondPath.erase(secondPath.begin());
+
+    firstPath.insert(firstPath.end(), secondPath.begin(), secondPath.end());
+    double totalTime = firstTime + secondTime;
+
+    std::cout << "Best Path with Include: ";
+    for (size_t i = 0; i < firstPath.size(); i++) {
+        std::cout << firstPath[i];
+        if (i < firstPath.size() - 1) {
             std::cout << "->";
         }
     }
-    std::cout << "\nTime: " << time << std::endl;
-
+    std::cout << "\nTotal Time: " << totalTime << std::endl;
 }
 
